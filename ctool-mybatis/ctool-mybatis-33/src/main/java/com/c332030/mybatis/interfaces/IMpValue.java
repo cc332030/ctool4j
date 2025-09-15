d5e10e2379d4f21d2baf076de568dbfe8669f88d
c332030 <c332030@c332030.com>
2025-09-15 09:58:31 +0800

    test: ctool-mybatis-base

---
+++ ctool-mybatis/ctool-mybatis-33/src/main/java/com/c332030/mybatis/interfaces/IMpValue.java
@@ -0,0 +1,17 @@
+package com.c332030.mybatis.interfaces;
+
+import com.baomidou.mybatisplus.core.enums.IEnum;
+import com.c332030.core.interfaces.IValue;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * Description: IMpValue
+ * </p>
+ *
+ * @since 2025/9/15
+ */
+public interface IMpValue<T extends Serializable> extends IValue<T>, IEnum<T> {
+
+}
