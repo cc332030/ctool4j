65ab9866408cd04cfba963e5ff9ad246882f7fc9
c332030 <c332030@c332030.com>
2025-09-16 14:45:06 +0800

    feat: CAssert string

--- ctool-core/src/main/java/com/c332030/ctool/core/lang/CAssert.java
+++ ctool-core/src/main/java/com/c332030/ctool/core/lang/CAssert.java
@@ -1,6 +1,10 @@
 package com.c332030.ctool.core.lang;
 
+import cn.hutool.core.util.StrUtil;
+import com.c332030.ctool.core.exception.CExceptionUtils;
 import lombok.experimental.UtilityClass;
+
+import java.util.function.Supplier;
 
 /**
  * <p>
@@ -12,6 +16,28 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CAssert {
 
+    public void notEmpty(String value, Supplier<String> messageSupplier) {
+        if(StrUtil.isEmpty(value)) {
+            CExceptionUtils.newBusinessException(messageSupplier.get());
+        }
+    }
 
+    public void notEmpty(String value, String message) {
+        if(StrUtil.isEmpty(value)) {
+            CExceptionUtils.newBusinessException(message);
+        }
+    }
+
+    public void notBlank(String value, Supplier<String> messageSupplier) {
+        if(StrUtil.isBlank(value)) {
+            CExceptionUtils.newBusinessException(messageSupplier.get());
+        }
+    }
+
+    public void notBlank(String value, String message) {
+        if(StrUtil.isBlank(value)) {
+            CExceptionUtils.newBusinessException(message);
+        }
+    }
 
 }
