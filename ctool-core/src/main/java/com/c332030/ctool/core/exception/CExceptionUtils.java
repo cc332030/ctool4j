4c8976d5b3452b3f5cf80aa6e04c82cc039272ea
c332030 <c332030@c332030.com>
2025-09-16 14:42:39 +0800

    feat: CExceptionUtils ignore

--- ctool-core/src/main/java/com/c332030/ctool/core/exception/CExceptionUtils.java
+++ ctool-core/src/main/java/com/c332030/ctool/core/exception/CExceptionUtils.java
@@ -1,6 +1,9 @@
 package com.c332030.ctool.core.exception;
 
+import com.c332030.ctool.core.function.CRunnable;
+import com.c332030.ctool.core.function.CSupplier;
 import com.c332030.ctool.core.util.CSpiUtils;
+import lombok.CustomLog;
 import lombok.experimental.UtilityClass;
 
 import java.util.function.BiFunction;
@@ -12,6 +15,7 @@ import java.util.function.BiFunction;
  *
  * @since 2025/9/14
  */
+@CustomLog
 @UtilityClass
 public class CExceptionUtils {
 
@@ -30,6 +34,31 @@ public class CExceptionUtils {
     @SuppressWarnings("unchecked")
     public <T extends Throwable> T newBusinessException(String message, Throwable cause) {
         return (T) getBusinessExceptionFunction().apply(message, cause);
+    }
+
+    public void ignore(CRunnable runnable) {
+        ignore(runnable, "处理失败");
+    }
+
+    public void ignore(CRunnable runnable, String message) {
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            log.error(message, e);
+        }
+    }
+
+    public <T> T ignore(CSupplier<T> supplier) {
+        return ignore(supplier, "处理失败");
+    }
+
+    public <T> T ignore(CSupplier<T> supplier, String message) {
+        try {
+            return CSupplier.get(supplier);
+        } catch (Exception e) {
+            log.error(message, e);
+            return null;
+        }
     }
 
 }
