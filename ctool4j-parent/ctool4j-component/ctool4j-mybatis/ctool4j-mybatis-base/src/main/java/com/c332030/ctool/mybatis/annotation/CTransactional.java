19c40858644338a00afc7f849c9c1978bf0f1203
c332030 <c332030@c332030.com>
2025-09-21 21:03:10 +0800

    feat: CTransactional

---
+++ ctool4j-parent/ctool4j-component/ctool4j-mybatis/ctool4j-mybatis-base/src/main/java/com/c332030/ctool/mybatis/annotation/CTransactional.java
@@ -0,0 +1,37 @@
+package com.c332030.ctool.mybatis.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>
+ * Description: CTransactional
+ * </p>
+ *
+ * @since 2025/9/21
+ */
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Transactional
+public @interface CTransactional {
+
+    @AliasFor(annotation = Transactional.class)
+    Propagation propagation() default Propagation.REQUIRED;
+
+    @AliasFor(annotation = Transactional.class)
+    Isolation isolation() default Isolation.DEFAULT;
+
+    @AliasFor(annotation = Transactional.class)
+    boolean readOnly() default false;
+
+    @AliasFor(annotation = Transactional.class)
+    Class<? extends Throwable>[] rollbackFor() default Exception.class;
+
+}
