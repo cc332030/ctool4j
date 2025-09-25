dd4073fed9d20e43fa15d04d9d245dffd186d9ae
c332030 <c332030@c332030.com>
2025-09-25 14:31:31 +0800

    feat: CServletUtils

---
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/util/CServletUtils.java
@@ -0,0 +1,15 @@
+package com.c332030.ctool4j.web.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * <p>
+ * Description: CServletUtils
+ * </p>
+ *
+ * @since 2025/9/25
+ */
+@UtilityClass
+public class CServletUtils {
+
+}
