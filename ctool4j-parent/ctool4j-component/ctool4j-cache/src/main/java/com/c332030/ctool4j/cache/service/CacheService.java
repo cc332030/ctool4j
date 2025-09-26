2836dd9c96ac700437dfe9e887c8fee54014f0e2
c332030 <c332030@c332030.com>
2025-09-26 17:36:08 +0800

    feat: CacheService

---
+++ ctool4j-parent/ctool4j-component/ctool4j-cache/src/main/java/com/c332030/ctool4j/cache/service/CacheService.java
@@ -0,0 +1,22 @@
+package com.c332030.ctool4j.cache.service;
+
+import com.c332030.ctool4j.redis.service.CRedisService;
+import lombok.AllArgsConstructor;
+import lombok.CustomLog;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * Description: CacheService
+ * </p>
+ *
+ * @since 2025/9/26
+ */
+@CustomLog
+@Service
+@AllArgsConstructor
+public class CacheService {
+
+    CRedisService redisService;
+
+}
