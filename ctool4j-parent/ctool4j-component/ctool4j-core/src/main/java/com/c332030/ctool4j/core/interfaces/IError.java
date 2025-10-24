6fe0190e7296c1f4bf3f867f94d7af06794cefc9
c332030 <c332030@c332030.com>
2025-10-24 11:15:04 +0800

    feat: IError

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/interfaces/IError.java
@@ -0,0 +1,16 @@
+package com.c332030.ctool4j.core.interfaces;
+
+/**
+ * <p>
+ * Description: IError
+ * </p>
+ *
+ * @since 2025/10/24
+ */
+public interface IError {
+
+    String getErrorCode();
+
+    String getErrorMsg();
+
+}
