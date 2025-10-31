d1c90452c0e9200e821a679f9b7e1ce830d1b9e3
c332030 <c332030@c332030.com>
2025-10-31 14:00:33 +0800

    feat: CPageUtils

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CPageUtils.java
@@ -0,0 +1,51 @@
+package com.c332030.ctool4j.core.util;
+
+import cn.hutool.core.collection.CollUtil;
+import com.c332030.ctool4j.core.function.CConsumer;
+import com.c332030.ctool4j.core.function.CFunction;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import lombok.var;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Description: CPageUtils
+ * </p>
+ *
+ * @since 2025/10/31
+ */
+@UtilityClass
+public class CPageUtils {
+
+    public static final Integer DEFAULT_LIST_PAGE_SIZE = 10;
+
+    public static final Integer DEFAULT_JOB_PAGE_SIZE = 1000;
+
+    @SneakyThrows
+    public <T> void queryThenDo(
+            CFunction<Integer, List<T>> queryFunction,
+            CConsumer<T> doSth
+    ) {
+
+        var start = 1;
+        while (true) {
+
+            val list = queryFunction.apply(start);
+            if(CollUtil.isEmpty(list)) {
+                break;
+            }
+
+            for (T t : list) {
+                doSth.accept(t);
+            }
+
+            start++;
+
+        }
+
+    }
+
+}
