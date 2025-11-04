99b06dea10eb3d436415c017ce497519d769ca3e
c332030 <c332030@c332030.com>
2025-11-04 16:23:51 +0800

    feat: RedisService 校验 key value

--- ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/service/ICRedisService.java
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/service/ICRedisService.java
@@ -1,9 +1,13 @@
 package com.c332030.ctool4j.redis.service;
 
+import com.c332030.ctool4j.core.function.CFunction;
+import lombok.SneakyThrows;
+import lombok.val;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 import java.time.Duration;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,25 +21,71 @@ public interface ICRedisService<K, V> {
 
     RedisTemplate<K, V> getRedisTemplate();
 
+    default boolean isInvalidKey(K key) {
+        return Objects.isNull(key);
+    }
+
+    default boolean isInvalidValue(V value) {
+        return Objects.isNull(value);
+    }
+
     default ValueOperations<K, V> opsForValue() {
         return getRedisTemplate().opsForValue();
     }
 
     default void setValue(K key, V value) {
+
+        if(isInvalidKey(key) || isInvalidValue(value)) {
+            return;
+        }
+
         opsForValue().set(key, value);
     }
     default void setValue(K key, V value, long timeout, TimeUnit unit) {
+
+        if(isInvalidKey(key)
+                || isInvalidValue(value)
+                || timeout <= 0
+        ) {
+            return;
+        }
+
         opsForValue().set(key, value, timeout, unit);
     }
     default void setValue(K key, V value, Duration timeout) {
+
+        if(isInvalidKey(key) || isInvalidValue(value)) {
+            return;
+        }
+
         opsForValue().set(key, value, timeout);
     }
 
     default V getValue(K key) {
+
+        if(isInvalidKey(key)) {
+            return null;
+        }
+
         return opsForValue().get(key);
     }
 
+    @SneakyThrows
+    default <T> T getValue(K key, CFunction<V, T> function) {
+
+        val value = getValue(key);
+        if(isInvalidValue(value)) {
+            return null;
+        }
+
+        return function.apply(value);
+    }
+
     default void delete(K key) {
+
+        if(isInvalidKey(key)) {
+            return;
+        }
         getRedisTemplate().delete(key);
     }
 
--- ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/service/impl/CStringStringRedisService.java
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/service/impl/CStringStringRedisService.java
@@ -1,5 +1,6 @@
 package com.c332030.ctool4j.redis.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.c332030.ctool4j.redis.service.CAbstractRedisService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,15 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class CStringStringRedisService extends CAbstractRedisService<String, String> {
+
+    @Override
+    public boolean isInvalidKey(String key) {
+        return StrUtil.isBlank(key);
+    }
+
+    @Override
+    public boolean isInvalidValue(String value) {
+        return StrUtil.isBlank(value);
+    }
 
 }
