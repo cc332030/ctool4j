a023856ed87e97f3db72b5a168b2ab7834e9e000
c332030 <c332030@c332030.com>
2025-11-04 16:12:24 +0800

    feat: CStringStringRedisService

--- ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/service/impl/CStringStringRedisService.java
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/service/impl/CStringStringRedisService.java
@@ -1,6 +1,7 @@
 package com.c332030.ctool4j.redis.service.impl;
 
 import com.c332030.ctool4j.redis.service.CAbstractRedisService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
  * @since 2025/11/4
  */
 @Service
+@AllArgsConstructor
 public class CStringStringRedisService extends CAbstractRedisService<String, String> {
 
 }
