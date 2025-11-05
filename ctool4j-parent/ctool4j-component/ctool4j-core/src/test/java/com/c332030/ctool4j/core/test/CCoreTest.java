7dee859773a3ea7004ebfdd5bb23bef8a2f43b51
c332030 <c332030@c332030.com>
2025-11-05 09:08:08 +0800

    test: CCoreTest

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/test/java/com/c332030/ctool4j/core/test/CCoreTest.java
@@ -0,0 +1,19 @@
+package com.c332030.ctool4j.core.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p>
+ * Description: CCoreTest
+ * </p>
+ *
+ * @since 2025/11/5
+ */
+public class CCoreTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
