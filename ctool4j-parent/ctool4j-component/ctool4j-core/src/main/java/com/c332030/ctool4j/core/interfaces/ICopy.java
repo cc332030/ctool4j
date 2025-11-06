bc48bc54315ee4166bf2133c7129c68b7fff9809
c332030 <c332030@c332030.com>
2025-11-06 10:24:19 +0800

    feat: ICopy

--- ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/interfaces/ICopy.java
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/interfaces/ICopy.java
@@ -11,6 +11,22 @@ import com.c332030.ctool4j.core.util.CBeanUtils;
  */
 public interface ICopy {
 
+    /**
+     * 拷贝成指定类型的对象
+     * @param t 目标对象
+     * @return 目标对象
+     * @param <T> 目标对象类型
+     */
+    default <T> T copy(T t) {
+        return CBeanUtils.copy(this, t);
+    }
+
+    /**
+     * 拷贝成指定类型的对象
+     * @param tClass 目标对象类型
+     * @return 目标对象
+     * @param <T> 目标对象类型
+     */
     default <T> T copy(Class<T> tClass) {
         return CBeanUtils.copy(this, tClass);
     }
