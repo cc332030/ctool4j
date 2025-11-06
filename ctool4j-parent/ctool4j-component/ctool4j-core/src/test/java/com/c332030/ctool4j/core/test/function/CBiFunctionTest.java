d34288022e0e6d5f5629ce15fe64b20432bd6b50
c332030 <c332030@c332030.com>
2025-11-07 00:00:59 +0800

    test: CBiFunctionTest CTriFunctionTest

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/test/java/com/c332030/ctool4j/core/test/function/CBiFunctionTest.java
@@ -0,0 +1,35 @@
+package com.c332030.ctool4j.core.test.function;
+
+import com.c332030.ctool4j.core.function.CBiFunction;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p>
+ * Description: CBiFunctionTest
+ * </p>
+ *
+ * @since 2025/11/6
+ */
+public class CBiFunctionTest {
+
+    @Test
+    @SneakyThrows
+    public void first() {
+
+        Assertions.assertEquals(1, CBiFunction.FIRST.apply(1, 2));
+        Assertions.assertEquals("1", CBiFunction.FIRST.apply("1", "2"));
+
+    }
+
+    @Test
+    @SneakyThrows
+    public void second() {
+
+        Assertions.assertEquals(2, CBiFunction.SECOND.apply(1, 2));
+        Assertions.assertEquals("2", CBiFunction.SECOND.apply("1", "2"));
+
+    }
+
+}
---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/test/java/com/c332030/ctool4j/core/test/function/CTriFunctionTest.java
@@ -0,0 +1,44 @@
+package com.c332030.ctool4j.core.test.function;
+
+import com.c332030.ctool4j.core.function.CTriFunction;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p>
+ * Description: CTriFunctionTest
+ * </p>
+ *
+ * @since 2025/11/6
+ */
+public class CTriFunctionTest {
+
+    @Test
+    @SneakyThrows
+    public void first() {
+
+        Assertions.assertEquals(1, CTriFunction.FIRST.apply(1, 2, 3));
+        Assertions.assertEquals("1", CTriFunction.FIRST.apply("1", "2", "3"));
+
+    }
+
+    @Test
+    @SneakyThrows
+    public void second() {
+
+        Assertions.assertEquals(2, CTriFunction.SECOND.apply(1, 2, 3));
+        Assertions.assertEquals("2", CTriFunction.SECOND.apply("1", "2", "3"));
+
+    }
+
+    @Test
+    @SneakyThrows
+    public void third() {
+
+        Assertions.assertEquals(3, CTriFunction.THIRD.apply(1, 2, 3));
+        Assertions.assertEquals("3", CTriFunction.THIRD.apply("1", "2", "3"));
+
+    }
+
+}
