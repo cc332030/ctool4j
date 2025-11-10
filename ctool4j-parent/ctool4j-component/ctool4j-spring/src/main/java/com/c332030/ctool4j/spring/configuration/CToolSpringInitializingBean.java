fd08688fbd4fa5279b16118263664ff069c74c1e
c332030 <c332030@c332030.com>
2025-11-10 15:02:57 +0800

    feat: CToolSpringInitializingBean

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/configuration/CToolSpringInitializingBean.java
@@ -0,0 +1,21 @@
+package com.c332030.ctool4j.spring.configuration;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Description: CToolSpringInitializingBean
+ * </p>
+ *
+ * @since 2025/11/10
+ */
+@Component
+public class CToolSpringInitializingBean implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+}
