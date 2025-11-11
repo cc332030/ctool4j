d49325494e11ab87ad382d95e09d5e6742f9d817
c332030 <c332030@c332030.com>
2025-11-11 09:47:07 +0800

    feat: getColumnsSql

--- ctool4j-parent/ctool4j-component/ctool4j-db/src/main/java/com/c332030/ctool4j/db/util/CSqlUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-db/src/main/java/com/c332030/ctool4j/db/util/CSqlUtils.java
@@ -1,10 +1,16 @@
 package com.c332030.ctool4j.db.util;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.func.Func1;
 import cn.hutool.core.lang.func.LambdaUtil;
 import cn.hutool.core.util.StrUtil;
 import com.c332030.ctool4j.core.util.CStrUtils;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -37,6 +43,38 @@ public class CSqlUtils {
      */
     public String toColumnName(String fieldName) {
         return StrUtil.toUnderlineCase(fieldName);
+    }
+
+    private String getAliasName(String column, String alias) {
+
+        if(StrUtil.isBlank(alias)
+                || BooleanUtils.TRUE.equalsIgnoreCase(column)
+                || BooleanUtils.FALSE.equalsIgnoreCase(column)
+        ){
+            return column;
+        }
+
+        return alias + "." + column;
+    }
+
+    /**
+     * 获取字段 sql
+     * @param funcList 属性 lambda 列表
+     * @param alias 别名
+     * @return sql
+     * @param <T> 泛型
+     */
+    public <T> String getColumnsSql(Collection<Func1<T, ?>> funcList, String alias) {
+
+        if (CollUtil.isEmpty(funcList)) {
+            return StringUtils.EMPTY;
+        }
+
+        return funcList.stream()
+                .map(LambdaUtil::getFieldName)
+                .map(CSqlUtils::toColumnName)
+                .map(column -> getAliasName(column, alias))
+                .collect(Collectors.joining(","));
     }
 
     /**
