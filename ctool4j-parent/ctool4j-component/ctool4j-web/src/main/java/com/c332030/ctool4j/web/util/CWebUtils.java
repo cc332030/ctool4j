9fe21f23ce7fb6bfb8a7a96dcdda3e90a193c6d5
c332030 <c332030@c332030.com>
2025-11-17 15:05:24 +0800

    feat: getContentDispositionValue

--- ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/util/CWebUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/util/CWebUtils.java
@@ -1,5 +1,7 @@
 package com.c332030.ctool4j.web.util;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * <p>
  * Description: CWebUtils
@@ -7,6 +9,11 @@ package com.c332030.ctool4j.web.util;
  *
  * @since 2025/9/28
  */
+@UtilityClass
 public class CWebUtils {
+
+    public String getContentDispositionValue(String filename) {
+        return "attachment;filename=" + filename;
+    }
 
 }
