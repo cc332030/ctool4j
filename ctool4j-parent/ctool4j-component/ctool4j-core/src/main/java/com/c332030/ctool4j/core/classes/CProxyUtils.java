a9e32fdfad091983e5438a0528c7ed5a81cc1e81
c332030 <c332030@c332030.com>
2025-11-22 11:54:50 +0800

    feat: CProxyUtils

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/classes/CProxyUtils.java
@@ -0,0 +1,15 @@
+package com.c332030.ctool4j.core.classes;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * <p>
+ * Description: CProxyUtils
+ * </p>
+ *
+ * @since 2025/11/22
+ */
+@UtilityClass
+public class CProxyUtils {
+
+}
