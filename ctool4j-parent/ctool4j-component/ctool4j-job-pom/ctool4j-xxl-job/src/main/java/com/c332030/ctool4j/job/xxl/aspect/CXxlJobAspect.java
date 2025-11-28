1c74b6230a2c4a559971d18d3578c876ae64954b
c332030 <c332030@c332030.com>
2025-11-28 10:23:57 +0800

    feat: jobParam info

--- ctool4j-parent/ctool4j-component/ctool4j-job-pom/ctool4j-xxl-job/src/main/java/com/c332030/ctool4j/job/xxl/aspect/CXxlJobAspect.java
+++ ctool4j-parent/ctool4j-component/ctool4j-job-pom/ctool4j-xxl-job/src/main/java/com/c332030/ctool4j/job/xxl/aspect/CXxlJobAspect.java
@@ -55,7 +55,7 @@ public class CXxlJobAspect {
 
                 val jobParam = CXxlJobUtils.getJobParam();
                 if(StrUtil.isNotBlank(jobParam)) {
-                    log.debug("jobParam: {}", jobParam);
+                    log.info("jobParam: {}", jobParam);
                     args[0] = jobParam;
                 }
             }
