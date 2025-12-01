82039ed0be95e9fe3c72fc4b521cdda948d25088
c332030 <c332030@c332030.com>
2025-12-01 18:55:11 +0800

    feat: CFeignClientAspect

--- ctool4j-parent/ctool4j-component/ctool4j-feign/src/main/java/com/c332030/ctool4j/feign/configuration/CFeignConfiguration.java
+++ ctool4j-parent/ctool4j-component/ctool4j-feign/src/main/java/com/c332030/ctool4j/feign/configuration/CFeignConfiguration.java
@@ -1,9 +1,7 @@
 package com.c332030.ctool4j.feign.configuration;
 
-import com.c332030.ctool4j.feign.client.CFeignClient;
 import com.c332030.ctool4j.feign.config.CFeignLogConfig;
 import com.c332030.ctool4j.feign.interceptor.CFeignInterceptor;
-import feign.Client;
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +19,6 @@ public class CFeignConfiguration {
     @Bean
     public RequestInterceptor requestInterceptor(CFeignLogConfig feignLogConfig) {
         return new CFeignInterceptor(feignLogConfig);
-    }
-
-    @Bean
-    public CFeignClient feignClient(Client client, CFeignLogConfig feignLogConfig) {
-        return new CFeignClient(client, feignLogConfig);
     }
 
 }
