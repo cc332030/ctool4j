64cf52a70ba54ffb95f94ce1b2faa8636c1df763
c332030 <c332030@c332030.com>
2025-12-04 15:56:33 +0800

    feat: CValueUtils

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CValueUtils.java
@@ -0,0 +1,23 @@
+package com.c332030.ctool4j.core.util;
+
+import com.c332030.ctool4j.definition.function.CConsumer;
+import com.c332030.ctool4j.definition.interfaces.IValue;
+import lombok.experimental.UtilityClass;
+
+/**
+ * <p>
+ * Description: CValueUtils
+ * </p>
+ *
+ * @since 2025/12/4
+ */
+@UtilityClass
+public class CValueUtils {
+
+    public <T> void setValue(IValue<T> iValue, CConsumer<T> consumer) {
+        if(null != iValue) {
+            consumer.accept(iValue.getValue());
+        }
+    }
+
+}
