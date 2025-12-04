5b3b4f67b0d2a8d0771227c13bbe963ee09339d2
c332030 <c332030@c332030.com>
2025-12-04 16:35:18 +0800

    feat: countByValues

--- ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-33/src/main/java/com/c332030/ctool4j/mybatisplus/service/ICService.java
+++ ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-33/src/main/java/com/c332030/ctool4j/mybatisplus/service/ICService.java
@@ -1,6 +1,11 @@
 package com.c332030.ctool4j.mybatisplus.service;
 
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.c332030.ctool4j.core.util.CCollUtils;
+import lombok.val;
+
+import java.util.Collection;
 
 /**
  * <p>
@@ -13,16 +18,36 @@ public interface ICService<T> extends ICBaseService<T> {
 
     default Integer countByValue(T entity, SFunction<T, ?> column){
         if(null == entity) {
-            return null;
+            return 0;
         }
         return countByValue(column, column.apply(entity));
     }
     default Integer countByValue(SFunction<T, ?> column, Object value){
         if(null == value) {
-            return null;
+            return 0;
         }
         return lambdaQuery()
             .eq(column, value)
+            .count();
+    }
+
+    default Integer countByValues(Collection<T> collection, SFunction<T, ?> column){
+
+        if(CollUtil.isEmpty(collection)) {
+            return 0;
+        }
+
+        val values = CCollUtils.convertSet(collection, column::apply);
+        return countByValues(column, values);
+    }
+
+    default Integer countByValues(SFunction<T, ?> column, Collection<?> values){
+
+        if(CollUtil.isEmpty(values)) {
+            return 0;
+        }
+        return lambdaQuery()
+            .in(column, values)
             .count();
     }
 
