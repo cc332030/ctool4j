59484ec3d53d96e239894ac8617d3f846eb9e9de
c332030 <c332030@c332030.com>
2025-12-07 11:12:46 +0800

    feat: CDateUtils

---
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CDateUtils.java
@@ -0,0 +1,15 @@
+package com.c332030.ctool4j.core.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * <p>
+ * Description: CDateUtils
+ * </p>
+ *
+ * @since 2025/12/7
+ */
+@UtilityClass
+public class CDateUtils {
+
+}
