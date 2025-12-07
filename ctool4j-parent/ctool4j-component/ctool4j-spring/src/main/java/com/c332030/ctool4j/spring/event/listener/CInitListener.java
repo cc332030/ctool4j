801161682648d4cbf63a4c819524e518bad09baf
c332030 <c332030@c332030.com>
2025-12-07 18:51:09 +0800

    docs: 文档

--- ctool4j-parent/ctool4j-component/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/event/listener/CInitListener.java
+++ ctool4j-parent/ctool4j-component/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/event/listener/CInitListener.java
@@ -12,11 +12,17 @@ import org.springframework.context.event.ContextRefreshedEvent;
  */
 public interface CInitListener extends InitializingBean, CurrentContextRefreshedListener {
 
+    /**
+     * 有的项目中，创建 Feign Client 的时候，会触发这个事件，但是这个时候，Bean 没有生成
+     */
     @Override
     default void afterPropertiesSet() {
         onInit();
     }
 
+    /**
+     * 刷新上下文时
+     */
     @Override
     default void onEvent(ContextRefreshedEvent event) {
         onInit();
