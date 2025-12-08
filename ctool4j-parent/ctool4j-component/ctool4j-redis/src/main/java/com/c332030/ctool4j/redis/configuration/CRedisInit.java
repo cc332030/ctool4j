8a4df489ff8c5d0cdc91d2a2d16f53ec5f9f7b5b
c332030 <c332030@c332030.com>
2025-12-08 20:26:15 +0800

    feat: ICMpLockService

---
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/configuration/CRedisInit.java
@@ -0,0 +1,24 @@
+package com.c332030.ctool4j.redis.configuration;
+
+import com.c332030.ctool4j.redis.service.impl.CLockService;
+import com.c332030.ctool4j.redis.util.CLockUtils;
+import com.c332030.ctool4j.spring.lifecycle.ICSpringInit;
+import com.c332030.ctool4j.spring.util.CSpringUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Description: CRedisInit
+ * </p>
+ *
+ * @since 2025/12/8
+ */
+@Component
+public class CRedisInit implements ICSpringInit {
+
+    @Override
+    public void onInit() {
+        CSpringUtils.wireBean(CLockService.class, CLockUtils::setLockService);
+    }
+
+}
--- ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/util/CLockUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/util/CLockUtils.java
@@ -1,7 +1,12 @@
 package com.c332030.ctool4j.redis.util;
 
 import com.c332030.ctool4j.definition.interfaces.IOperate;
+import com.c332030.ctool4j.redis.service.impl.CLockService;
+import lombok.Setter;
 import lombok.experimental.UtilityClass;
+
+import java.time.Duration;
+import java.util.function.Supplier;
 
 /**
  * <p>
@@ -16,14 +21,41 @@ public class CLockUtils {
 
     public static final String LOCK_STR = "lock";
 
+    @Setter
+    CLockService lockService;
+
+    /**
+     * 获取锁 key
+     * @param key 业务 key
+     * @return 锁 key
+     */
     public static String getLockKey(String key) {
         return key
                 + CRedisUtils.KEY_SEPARATOR
                 +  LOCK_STR;
     }
 
+    /**
+     * 获取锁 key
+     * @param clazz 业务类
+     * @param iOperate 操作
+     * @param key 业务 key
+     * @return 锁 key
+     */
     public static String getLockKey(Class<?> clazz, IOperate iOperate, Object key) {
         return getLockKey(CRedisUtils.getKey(clazz, iOperate, key));
+    }
+
+    /**
+     * 获取锁并做处理
+     * @param key lockKey
+     * @param waitDuration 等待时长
+     * @param valueSupplier 锁成功操作
+     * @return 锁成功操作结果
+     * @param <T> 返回结果类型
+     */
+    public <T> T tryLockThenRun(String key, Duration waitDuration, Supplier<T> valueSupplier) {
+        return lockService.tryLockThenRun(key, waitDuration, valueSupplier);
     }
 
 }
