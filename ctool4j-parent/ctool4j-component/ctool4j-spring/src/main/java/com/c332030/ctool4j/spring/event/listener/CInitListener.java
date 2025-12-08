b56f943d51d9735a71ec0d1cefa79db431fabe2c
c332030 <c332030@c332030.com>
2025-12-08 13:26:38 +0800

    feat: CInitListener

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/event/listener/CInitListener.java
@@ -0,0 +1,15 @@
+package com.c332030.ctool4j.spring.event.listener;
+
+import com.c332030.ctool4j.spring.lifecycle.ICSpringInit;
+
+/**
+ * <p>
+ * Description: CInitListener
+ * </p>
+ *
+ * @since 2025/12/8
+ */
+@Deprecated
+public interface CInitListener extends ICSpringInit {
+
+}
