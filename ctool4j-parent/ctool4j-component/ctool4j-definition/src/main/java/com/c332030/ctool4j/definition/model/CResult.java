1db617d6c001c37b9e75ae13fc7a930e2cbe51ff
c332030 <c332030@c332030.com>
2025-12-10 09:23:20 +0800

    feat: CResult

--- ctool4j-parent/ctool4j-component/ctool4j-definition/src/main/java/com/c332030/ctool4j/definition/model/CResult.java
+++ ctool4j-parent/ctool4j-component/ctool4j-definition/src/main/java/com/c332030/ctool4j/definition/model/CResult.java
@@ -1,13 +1,15 @@
 package com.c332030.ctool4j.definition.model;
 
+import cn.hutool.core.util.StrUtil;
 import com.c332030.ctool4j.definition.model.result.impl.CIntResult;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
 
 /**
  * <p>
- * Description: CIntResult
+ * Description: CResult
  * </p>
  *
  * @since 2025/5/13
@@ -17,5 +19,32 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 public class CResult<DATA> extends CIntResult<DATA> implements ICResult<Integer, DATA> {
+
+    public static <DATA> CResult<DATA> newInstance(Integer code, String message, DATA data) {
+        return CResult.<DATA>builder()
+            .code(code)
+            .msg(message)
+            .data(data)
+            .build();
+    }
+
+    public static <DATA> CResult<DATA> success() {
+        return success(null);
+    }
+
+    public static <DATA> CResult<DATA> success(DATA data) {
+        return newInstance(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), data);
+    }
+
+    public static <DATA> CResult<DATA> error(String message) {
+        return error(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            StrUtil.nullToDefault(message, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+        );
+    }
+
+    public static <DATA> CResult<DATA> error(Integer code, String message) {
+        return newInstance(code, message, null);
+    }
 
 }
