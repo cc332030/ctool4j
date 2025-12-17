3aea7b99f4e6446a56ba0605a3b41b6a038c6c12
c332030 <c332030@c332030.com>
2025-12-17 13:29:38 +0800

    feat: 启用验证

--- ctool4j-parent/ctool4j-component/ctool4j-doc-pom/ctool4j-doc-openapi2/src/main/java/com/c332030/ctool4j/doc/openapi2/configuration/OpenApi2Configuration.java
+++ ctool4j-parent/ctool4j-component/ctool4j-doc-pom/ctool4j-doc-openapi2/src/main/java/com/c332030/ctool4j/doc/openapi2/configuration/OpenApi2Configuration.java
@@ -1,19 +1,8 @@
 package com.c332030.ctool4j.doc.openapi2.configuration;
 
-import com.c332030.ctool4j.core.util.CArrUtils;
-import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
-import lombok.val;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.schema.ModelPropertyBuilderPlugin;
-import springfox.documentation.spi.schema.contexts.ModelPropertyContext;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.lang.annotation.Annotation;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
 /**
  * <p>
@@ -23,36 +12,7 @@ import java.lang.annotation.Annotation;
  * @since 2025/12/16
  */
 @Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
 public class OpenApi2Configuration {
-
-    private static final Class<? extends Annotation>[] REQUIREED_ANNOTATIONS = CArrUtils.getArr(
-        NotNull.class,
-        NotBlank.class,
-        NotEmpty.class
-    );
-
-    @Bean
-    public ModelPropertyBuilderPlugin requiredNotEmptyPlugin() {
-        return new ModelPropertyBuilderPlugin() {
-
-            @Override
-            public boolean supports(@NonNull DocumentationType documentationType) {
-                return true;
-            }
-
-            @Override
-            public void apply(ModelPropertyContext context) {
-
-                context.getBeanPropertyDefinition()
-                    .map(BeanPropertyDefinition::getField)
-                    .filter(field -> {
-                        val annotations = field.getAllAnnotations();
-                        return annotations.hasOneOf(REQUIREED_ANNOTATIONS);
-                    })
-                    .ifPresent(e -> context.getBuilder().required(true));
-
-            }
-        };
-    }
 
 }
