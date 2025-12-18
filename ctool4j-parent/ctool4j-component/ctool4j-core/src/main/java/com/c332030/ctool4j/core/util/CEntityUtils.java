9e43f85598eed6fe8e0e70986bb923405c948def
c332030 <c332030@c332030.com>
2025-12-18 11:04:48 +0800

    feat: CEntityUtils clear

--- ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CEntityUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CEntityUtils.java
@@ -17,7 +17,7 @@ public class CEntityUtils {
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICId<?> entity) {
+    public void clear(ICId<?> entity) {
         entity.setId(null);
     }
 
@@ -25,7 +25,7 @@ public class CEntityUtils {
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICCreateTime entity) {
+    public void clear(ICCreateTime entity) {
         entity.setCreateTime(null);
     }
 
@@ -33,7 +33,7 @@ public class CEntityUtils {
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICUpdateTime entity) {
+    public void clear(ICUpdateTime entity) {
         entity.setUpdateTime(null);
     }
 
@@ -41,16 +41,16 @@ public class CEntityUtils {
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICCreateUpdateTime entity) {
-        clearEntity((ICCreateTime)entity);
-        clearEntity((ICUpdateTime)entity);
+    public void clear(ICCreateUpdateTime entity) {
+        clear((ICCreateTime)entity);
+        clear((ICUpdateTime)entity);
     }
 
     /**
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICCreateBy entity) {
+    public void clear(ICCreateBy entity) {
         entity.setCreateBy(null);
         entity.setCreateById(null);
     }
@@ -59,7 +59,7 @@ public class CEntityUtils {
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICUpdateBy entity) {
+    public void clear(ICUpdateBy entity) {
         entity.setUpdateBy(null);
         entity.setUpdateById(null);
     }
@@ -68,36 +68,36 @@ public class CEntityUtils {
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICCreateUpdateBy entity) {
-        clearEntity((ICCreateBy)entity);
-        clearEntity((ICUpdateBy)entity);
+    public void clear(ICCreateUpdateBy entity) {
+        clear((ICCreateBy)entity);
+        clear((ICUpdateBy)entity);
     }
 
     /**
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(ICCreateUpdateByAndTime entity) {
-        clearEntity((ICCreateUpdateBy)entity);
-        clearEntity((ICCreateUpdateTime)entity);
+    public void clear(ICCreateUpdateByAndTime entity) {
+        clear((ICCreateUpdateBy)entity);
+        clear((ICCreateUpdateTime)entity);
     }
 
     /**
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(CBaseTimeEntity entity) {
-        clearEntity((ICId<?>) entity);
-        clearEntity((ICCreateUpdateTime)entity);
+    public void clear(CBaseTimeEntity entity) {
+        clear((ICId<?>) entity);
+        clear((ICCreateUpdateTime)entity);
     }
 
     /**
      * 清空实体
      * @param entity 实体
      */
-    public void clearEntity(CBaseEntity entity) {
-        clearEntity((ICId<?>) entity);
-        clearEntity((ICCreateUpdateByAndTime)entity);
+    public void clear(CBaseEntity entity) {
+        clear((ICId<?>) entity);
+        clear((ICCreateUpdateByAndTime)entity);
     }
 
 }
