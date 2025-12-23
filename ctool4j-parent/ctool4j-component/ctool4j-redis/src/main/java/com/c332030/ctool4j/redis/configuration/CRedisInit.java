fafcfa673ce26411eb96d5b6393158527db2ea1a
c332030 <c332030@c332030.com>
2025-12-23 15:56:46 +0800

    feat: setIfLager

--- ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/configuration/CRedisInit.java
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/configuration/CRedisInit.java
@@ -1,7 +1,9 @@
 package com.c332030.ctool4j.redis.configuration;
 
 import com.c332030.ctool4j.redis.service.impl.CLockService;
+import com.c332030.ctool4j.redis.service.impl.CObjectValueRedisService;
 import com.c332030.ctool4j.redis.util.CLockUtils;
+import com.c332030.ctool4j.redis.util.CRedisUtils;
 import com.c332030.ctool4j.spring.lifecycle.ICSpringInit;
 import com.c332030.ctool4j.spring.util.CSpringUtils;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,10 @@ public class CRedisInit implements ICSpringInit {
 
     @Override
     public void onInit() {
+
+        CSpringUtils.wireBean(CObjectValueRedisService.class, CRedisUtils::setRedisService);
         CSpringUtils.wireBean(CLockService.class, CLockUtils::setLockService);
+
     }
 
 }
--- ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/util/CRedisUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-redis/src/main/java/com/c332030/ctool4j/redis/util/CRedisUtils.java
@@ -1,8 +1,15 @@
 package com.c332030.ctool4j.redis.util;
 
 import com.c332030.ctool4j.definition.interfaces.IOperate;
+import com.c332030.ctool4j.redis.service.impl.CObjectValueRedisService;
 import com.c332030.ctool4j.spring.util.CSpringUtils;
+import lombok.Setter;
 import lombok.experimental.UtilityClass;
+import lombok.val;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+
+import java.util.Collections;
 
 /**
  * <p>
@@ -14,7 +21,10 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CRedisUtils {
 
-    public static final String KEY_SEPARATOR = ":";
+    public final String KEY_SEPARATOR = ":";
+
+    @Setter
+    CObjectValueRedisService redisService;
 
     public static String getKey(Class<?> clazz, IOperate iOperate, Object key) {
         return CSpringUtils.getApplicationName()
@@ -25,6 +35,33 @@ public class CRedisUtils {
                 + KEY_SEPARATOR
                 + key
                 ;
+    }
+
+    private static final String SET_IF_LAGER = "local current = redis.call('GET',  KEYS[1])\n" +
+        "local currentNum = 0  -- 默认值\n" +
+        "if current ~= false then  -- 判断非空 [3]()\n" +
+        "    currentNum = tonumber(current)  -- 显式转换 [2]()\n" +
+        "end\n" +
+        "if tonumber(ARGV[1]) > currentNum then\n" +
+        "    redis.call('SET',  KEYS[1], ARGV[1])\n" +
+        "    return 1  -- 更新成功\n" +
+        "end\n" +
+        "return 0";
+
+    private static final RedisScript<Long> SET_IF_LAGER_SCRIPT = new DefaultRedisScript<>(SET_IF_LAGER, Long.class);
+
+    public boolean setIfLager(String key, Number value) {
+
+        if(null == value) {
+            return false;
+        }
+
+        val result = redisService.getRedisTemplate().execute(
+            SET_IF_LAGER_SCRIPT,
+            Collections.singletonList(key),
+            value
+        );
+        return result == 1;
     }
 
 }
