909d5ee9d91a0f8dc7cc8709172ff3abe3bad757
c332030 <c332030@c332030.com>
2025-12-29 13:42:22 +0800

    feat: CRequestHeaderEnum

---
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/enums/CRequestHeaderEnum.java
@@ -0,0 +1,37 @@
+package com.c332030.ctool4j.web.enums;
+
+import com.c332030.ctool4j.core.interfaces.ICRequestHeader;
+import com.c332030.ctool4j.definition.enums.CDataTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * <p>
+ * Description: RequestHeaderEnum
+ * </p>
+ *
+ * @author c332030
+ * @since 2024/3/21
+ */
+@Getter
+@AllArgsConstructor
+public enum CRequestHeaderEnum implements ICRequestHeader {
+
+    AUTHORIZATION("鉴权", CDataTypeEnum.STRING, false),
+
+    ACCEPT_LANGUAGE("语言", CDataTypeEnum.STRING, false),
+
+    ACCEPT("内容格式", CDataTypeEnum.STRING, false),
+
+    ;
+
+    /**
+     * 描述
+     */
+    private final String text;
+
+    private final CDataTypeEnum dataType;
+
+    private final boolean required;
+
+}
