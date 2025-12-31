9ed076555f85b7f1147cb477198357b9a377628a
c332030 <c332030@c332030.com>
2025-12-31 09:19:32 +0800

    feat: remove getDocket

--- ctool4j-parent/ctool4j-component/ctool4j-doc-pom/ctool4j-doc-openapi2/src/main/java/com/c332030/ctool4j/doc/openapi2/util/CSpringFoxUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-doc-pom/ctool4j-doc-openapi2/src/main/java/com/c332030/ctool4j/doc/openapi2/util/CSpringFoxUtils.java
@@ -3,8 +3,6 @@ package com.c332030.ctool4j.doc.openapi2.util;
 import com.c332030.ctool4j.core.interfaces.ICRequestHeader;
 import com.c332030.ctool4j.core.util.CCollUtils;
 import lombok.experimental.UtilityClass;
-import lombok.val;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.Parameter;
@@ -13,8 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -31,23 +27,6 @@ public class CSpringFoxUtils {
     public Docket getDocketBuilder() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .forCodeGeneration(true);
-    }
-
-    public Docket getDocket(
-            String groupName, Collection<ICRequestHeader> headers,
-            Predicate<RequestHandler> apiSelector,
-            Predicate<String> pathSelector
-    ) {
-
-        val builder = getDocketBuilder()
-                .groupName(groupName)
-                .globalOperationParameters(globalParameterList(headers))
-                .select();
-
-        Optional.ofNullable(apiSelector).ifPresent(builder::apis);
-        Optional.ofNullable(pathSelector).ifPresent(builder::paths);
-
-        return builder.build();
     }
 
     public List<Parameter> globalParameterList(Collection<ICRequestHeader> headers) {
