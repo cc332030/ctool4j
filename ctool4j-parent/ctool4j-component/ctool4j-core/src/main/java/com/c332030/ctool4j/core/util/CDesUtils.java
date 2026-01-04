141fa88bca4e3ac1fd64e759d9a2b315c8664f68
c332030 <c332030@c332030.com>
2026-01-04 15:37:24 +0800

    feat: CBase64Utils CBase62Utils

--- ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CDesUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CDesUtils.java
@@ -1,13 +1,10 @@
 package com.c332030.ctool4j.core.util;
 
-import cn.hutool.core.codec.Base62;
-import cn.hutool.core.codec.Base64;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.symmetric.DES;
 import lombok.experimental.UtilityClass;
-import lombok.val;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -39,11 +36,11 @@ public class CDesUtils {
     }
 
     public String encryptStr64(String key, String plainText) {
-        return Base64.encode(encrypt(key, plainText));
+        return CBase64Utils.encode(encrypt(key,plainText));
     }
 
     public String encryptStr62(String key, String plainText) {
-        return Base62.encode(encrypt(key, plainText));
+        return CBase62Utils.encode(encrypt(key,plainText));
     }
 
     public String decrypt(String key, byte[] cipherBytes) {
@@ -56,25 +53,11 @@ public class CDesUtils {
     }
 
     public String decryptStr64(String key, String cipherText) {
-        if(StrUtil.isBlank(key)
-            || StrUtil.isBlank(cipherText)
-        ){
-            return null;
-        }
-
-        val bytes = Base64.decode(cipherText);
-        return decrypt(key, bytes);
+        return decrypt(key, CBase64Utils.decode(cipherText));
     }
 
     public String decryptStr62(String key, String cipherText) {
-        if(StrUtil.isBlank(key)
-            || StrUtil.isBlank(cipherText)
-        ){
-            return null;
-        }
-
-        val bytes = Base62.decode(cipherText);
-        return decrypt(key, bytes);
+        return decrypt(key, CBase62Utils.decode(cipherText));
     }
 
 }
