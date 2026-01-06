dafe1a8bdce3c7b831316ea6b373d1f74535f3b7
c332030 <c332030@c332030.com>
2026-01-06 18:20:54 +0800

    feat: mapper method

--- ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-base/src/main/java/com/c332030/ctool4j/mybatisplus/injector/CSqlMethod.java
+++ ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-base/src/main/java/com/c332030/ctool4j/mybatisplus/injector/CSqlMethod.java
@@ -1,5 +1,6 @@
 package com.c332030.ctool4j.mybatisplus.injector;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.enums.SqlMethod;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,22 +18,24 @@ import lombok.Getter;
 public enum CSqlMethod implements ISqlMethodEnum {
 
     INSERT_IGNORE(
-            "insertIgnore",
             "插入一条数据（如果存在则忽略）",
         SqlMethod.INSERT_ONE.getSql()
             .replaceAll("INSERT", "INSERT IGNORE")
     ),
 
     UPDATE_ALL_BY_ID(
-        "updateAllById",
         "根据ID 选择修改数据，数据为空则设置为空",
         SqlMethod.UPDATE_BY_ID.getSql()
     ),
 
     ;
 
-    private final String method;
-    private final String desc;
-    private final String sql;
+    final String desc;
+    final String sql;
+
+    @Override
+    public String getMethod() {
+        return StrUtil.toCamelCase(name());
+    }
 
 }
