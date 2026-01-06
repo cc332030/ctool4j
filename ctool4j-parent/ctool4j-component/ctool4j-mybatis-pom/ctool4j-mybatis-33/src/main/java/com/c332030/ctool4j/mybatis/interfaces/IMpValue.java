f4c1dcb03080f3ff0ed2ae5f8d03da83dde4bb6a
c332030 <c332030@c332030.com>
2026-01-06 16:31:44 +0800

    feat: IMpValue Deprecated

---
+++ ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-33/src/main/java/com/c332030/ctool4j/mybatis/interfaces/IMpValue.java
@@ -0,0 +1,15 @@
+package com.c332030.ctool4j.mybatis.interfaces;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * Description: IMpValue
+ * </p>
+ *
+ * @since 2026/1/6
+ */
+@Deprecated
+public interface IMpValue<T extends Serializable> extends ICMpValue<T>{
+
+}
