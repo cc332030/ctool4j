ebc8fb4abddda1f6728c8fbdb83b19a05fa9e07b
c332030 <c332030@c332030.com>
2026-01-06 09:44:16 +0800

    feat: 调整日志打印

--- ctool4j-parent/ctool4j-component/ctool4j-feign/src/main/java/com/c332030/ctool4j/feign/log/CFeignLogger.java
+++ ctool4j-parent/ctool4j-component/ctool4j-feign/src/main/java/com/c332030/ctool4j/feign/log/CFeignLogger.java
@@ -9,7 +9,9 @@ import com.c332030.ctool4j.core.log.CLogUtils;
 import com.c332030.ctool4j.core.util.CBoolUtils;
 import com.c332030.ctool4j.core.util.CCommUtils;
 import com.c332030.ctool4j.core.util.CThreadLocalUtils;
+import com.c332030.ctool4j.definition.function.CBiFunction;
 import com.c332030.ctool4j.feign.config.CFeignClientLogConfig;
+import com.c332030.ctool4j.feign.model.CFeignRequestLog;
 import com.c332030.ctool4j.feign.util.CFeignUtils;
 import feign.Logger;
 import feign.Request;
@@ -41,53 +43,44 @@ public class CFeignLogger extends Logger {
 
     static final CLog FEIGN_LOG = CLogUtils.getLog(FEIGN_LOG_STR);
 
-    static final ThreadLocal<Long> START_MILLS = new ThreadLocal<>();
+    static final ThreadLocal<CFeignRequestLog> REQUEST_LOG_THREAD_LOCAL =
+        ThreadLocal.withInitial(CFeignRequestLog::new);
 
     CFeignClientLogConfig config;
 
     @Override
     protected void logRequest(String configKey, Level logLevel, Request request) {
-        if(CBoolUtils.isTrue(config.getEnable())
-            && BooleanUtil.isTrue(config.getEnableCost())
-        ) {
-            START_MILLS.set(System.currentTimeMillis());
+        if(CBoolUtils.isTrue(config.getEnable())) {
+            val requestLog = REQUEST_LOG_THREAD_LOCAL.get();
+            if(BooleanUtil.isTrue(config.getEnableCost())) {
+                requestLog.setStartMills(System.currentTimeMillis());
+            }
+            if(enableLog(request)) {
+                val httpLog = getRequestLog(request);
+                requestLog.setHttpLog(httpLog);
+            }
         }
         super.logRequest(configKey, logLevel, request);
     }
 
     @Override
-    protected Response logAndRebufferResponse(String configKey, Level logLevel, Response response, long elapsedTime) throws IOException {
+    protected Response logAndRebufferResponse(String configKey, Level logLevel, Response response, long elapsedTime) {
+        return dealResponse(response, elapsedTime, this::dealResponseLog);
+    }
 
-        if(CBoolUtils.isTrue(config.getEnable())) {
-            try {
-                if(enableLog(response)) {
-                    return dealLog(response);
-                }
-            } catch (Throwable e) {
-                log.error("处理响应日志失败", e);
-            } finally {
-                if(BooleanUtil.isTrue(config.getEnableCost())) {
-                    val startMills = CThreadLocalUtils.getThenRemove(START_MILLS);
-                    if(null != startMills) {
-                        val cost = System.currentTimeMillis() - startMills;
-                        log.info("cost: {}, elapsedTime: {}", cost, elapsedTime);
-                    }
-                }
-            }
-        }
-
-        return response;
+    @Override
+    protected IOException logIOException(String configKey, Level logLevel, IOException ioe, long elapsedTime) {
+        return dealResponse(ioe, elapsedTime, this::dealErrorLog);
     }
 
     @Override
     protected void log(String configKey, String format, Object... args) {
-
+        log.warn("Don't call this log method");
     }
 
     @SneakyThrows
-    private boolean enableLog(Response response) {
+    private boolean enableLog(Request request) {
 
-        val request = response.request();
         val url = new URL(request.url());
         val host = url.getHost();
         val path = url.getPath();
@@ -114,10 +107,8 @@ public class CFeignLogger extends Logger {
         return CBoolUtils.isTrue(config.getLogAll());
     }
 
-    @SneakyThrows
-    private Response dealLog(Response response) {
+    private StringBuilder getRequestLog(Request request) {
 
-        val request = response.request();
         val method = request.httpMethod();
         val url = request.url();
 
@@ -129,21 +120,56 @@ public class CFeignLogger extends Logger {
         httpLog.append(url);
 
         val requestHeaders = request.headers();
-        printHeaders(httpLog, requestHeaders);
+        printHeaders(httpLog, requestHeaders, true);
         printBody(httpLog, requestHeaders, request.body(), "request");
+
+        return httpLog;
+    }
+
+    private <T> T dealResponse(T t, long elapsedTime, CBiFunction<T, StringBuilder, T> function) {
+
+        if(CBoolUtils.isTrue(config.getEnable())) {
+            val requestLog = CThreadLocalUtils.getThenRemove(REQUEST_LOG_THREAD_LOCAL);
+            try {
+                val httpLog = requestLog.getHttpLog();
+                if(null != httpLog) {
+                    return function.apply(t, httpLog);
+                }
+            } catch (Throwable e) {
+                log.error("处理响应日志失败", e);
+            } finally {
+                val startMills = requestLog.getStartMills();
+                if(null != startMills) {
+                    val cost = System.currentTimeMillis() - startMills;
+                    log.info("cost: {}, elapsedTime: {}", cost, elapsedTime);
+                }
+            }
+        }
+
+        return t;
+    }
+
+    private Response dealResponseLog(Response response, StringBuilder httpLog) {
 
         val responseHeaders = response.headers();
         val responseBodyBytes = getBodyBytes(response);
 
-        httpLog.append("\n");
-        printHeaders(httpLog, responseHeaders);
+        printHeaders(httpLog, responseHeaders, false);
         printBody(httpLog, responseHeaders, responseBodyBytes, "response");
         FEIGN_LOG.info("{}", httpLog);
 
         return CFeignUtils.newResponse(response, responseBodyBytes);
     }
 
-    @SneakyThrows
+    private IOException dealErrorLog(IOException ioException, StringBuilder httpLog) {
+
+        httpLog.append("\n\n");
+        httpLog.append(ioException.getMessage());
+        FEIGN_LOG.info("{}", httpLog);
+
+        return ioException;
+    }
+
     private byte[] getBodyBytes(Response response) {
         try {
             val inputStream = CObjUtils.convert(response.body(), Response.Body::asInputStream);
@@ -154,12 +180,19 @@ public class CFeignLogger extends Logger {
         }
     }
 
-    private void printHeaders(StringBuilder httpLog, Map<String, Collection<String>> headers) {
+    private void printHeaders(
+        StringBuilder httpLog,
+        Map<String, Collection<String>> headers,
+        boolean isRequest
+    ) {
 
         if(BooleanUtil.isTrue(config.getEnableHeader())) {
 
             val headerStr = CCommUtils.getFullHeaderStr(headers);
             if(StrUtil.isNotEmpty(headerStr)) {
+                if(!isRequest) {
+                    httpLog.append("\n");
+                }
                 httpLog.append("\n");
                 httpLog.append(CCommUtils.getFullHeaderStr(headers));
             }
