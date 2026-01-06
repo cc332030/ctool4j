a20b056442e1b52934b2e6628cdc75f6cfd023e3
c332030 <c332030@c332030.com>
2026-01-06 16:01:38 +0800

    feat: updateByIdIncludeNull

---
+++ ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-33/src/main/java/com/c332030/ctool4j/mybatisplus/injector/methods/UpdateByIdIncludeNullMethod.java
@@ -0,0 +1,38 @@
+package com.c332030.ctool4j.mybatisplus.injector.methods;
+
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.c332030.ctool4j.mybatisplus.injector.CAbstractMethod;
+import com.c332030.ctool4j.mybatisplus.injector.CSqlMethod;
+import lombok.val;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Description: UpdateByIdIncludeNullMethod
+ * </p>
+ *
+ * @since 2026/1/6
+ */
+@Component
+public class UpdateByIdIncludeNullMethod extends CAbstractMethod {
+
+    public UpdateByIdIncludeNullMethod() {
+        super(CSqlMethod.UPDATE_BY_ID_INCLUDE_NULL_METHOD);
+    }
+
+    @Override
+    public MappedStatement injectMappedStatement(Class<?> mapperClass, Class<?> modelClass, TableInfo tableInfo) {
+
+        val logicDelete = tableInfo.isLogicDelete();
+        val additional = optlockVersion(tableInfo) + tableInfo.getLogicDeleteSql(true, true);
+        val sql = String.format(
+            sqlMethod.getSql(), tableInfo.getTableName(),
+            sqlSet(logicDelete, false, tableInfo, false, ENTITY, ENTITY_DOT),
+            tableInfo.getKeyColumn(), ENTITY_DOT + tableInfo.getKeyProperty(), additional
+        );
+        val sqlSource = languageDriver.createSqlSource(configuration, sql, modelClass);
+        return addUpdateMappedStatement(mapperClass, modelClass, sqlMethod.getMethod(), sqlSource);
+    }
+
+}
--- ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-base/src/main/java/com/c332030/ctool4j/mybatisplus/injector/CSqlMethod.java
+++ ctool4j-parent/ctool4j-component/ctool4j-mybatis-pom/ctool4j-mybatis-base/src/main/java/com/c332030/ctool4j/mybatisplus/injector/CSqlMethod.java
@@ -21,6 +21,12 @@ public enum CSqlMethod implements ISqlMethodEnum {
             "<script>\nINSERT IGNORE INTO %s %s VALUES %s\n</script>"
     ),
 
+    UPDATE_BY_ID_INCLUDE_NULL_METHOD(
+        "updateByIdIncludeNullMethod",
+        "根据ID 选择修改数据，数据为空则设置为空",
+        "<script>\nUPDATE %s %s WHERE %s=#{%s} %s\n</script>"
+    ),
+
     ;
 
     private final String method;
