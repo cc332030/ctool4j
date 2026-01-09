a124935ecc26d4367f1155261b4946e804ca5ee7
c332030 <c332030@c332030.com>
2026-01-09 23:43:17 +0800

    feat: CWebMvcConfig

--- ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/configurer/CWebMvcConfig.java
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/configurer/CWebMvcConfig.java
@@ -3,7 +3,6 @@ package com.c332030.ctool4j.web.configurer;
 import com.c332030.ctool4j.core.util.CCollUtils;
 import com.c332030.ctool4j.web.interceptor.ICHandlerInterceptor;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +17,7 @@ import java.util.Collection;
  * @since 2025/9/28
  */
 @Configuration
-@AllArgsConstructor(onConstructor_ = @Autowired(required = false))
+@AllArgsConstructor
 public class CWebMvcConfig implements WebMvcConfigurer {
 
     Collection<ICHandlerInterceptor> icHandlerInterceptors;
