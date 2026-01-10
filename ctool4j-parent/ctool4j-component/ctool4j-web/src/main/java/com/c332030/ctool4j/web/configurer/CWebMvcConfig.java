00ccfb1ccc6a1acfb5d460bfa0f462a3bd66a953
c332030 <c332030@c332030.com>
2026-01-10 21:20:23 +0800

    feat: 修复错误

--- ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/configurer/CWebMvcConfig.java
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/configurer/CWebMvcConfig.java
@@ -1,8 +1,14 @@
 package com.c332030.ctool4j.web.configurer;
 
+import com.c332030.ctool4j.core.util.CCollUtils;
+import com.c332030.ctool4j.web.interceptor.ICHandlerInterceptor;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collection;
 
 /**
  * <p>
@@ -12,7 +18,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @since 2025/9/28
  */
 @Configuration
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor = @__(@Autowired(required = false)))
 public class CWebMvcConfig implements WebMvcConfigurer {
+
+    Collection<ICHandlerInterceptor> icHandlerInterceptors;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        CCollUtils.forEach(icHandlerInterceptors, registry::addInterceptor);
+
+    }
 
 }
