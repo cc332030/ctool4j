f983264b00ee8d6129415a571733af52faa386d0
c332030 <c332030@c332030.com>
2026-01-10 11:05:37 +0800

    feat: cors

--- ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/cors/util/CCorsUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/cors/util/CCorsUtils.java
@@ -46,7 +46,7 @@ public class CCorsUtils {
 
     public void handle(HttpServletRequest request, HttpServletResponse response) {
         try {
-            if(CBoolUtils.isTrue(config.getEnable())) {
+            if(null != config && CBoolUtils.isTrue(config.getEnable())) {
                 handleDo(request, response);
             }
         } catch (Throwable e) {
