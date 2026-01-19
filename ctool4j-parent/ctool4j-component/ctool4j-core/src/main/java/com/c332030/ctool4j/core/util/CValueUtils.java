8f01faaa9b22d1a1d5ebe74863bc37581c400261
c332030 <c332030@c332030.com>
2026-01-19 16:20:58 +0800

    feat: CValueUtils getValue

--- ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CValueUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-core/src/main/java/com/c332030/ctool4j/core/util/CValueUtils.java
@@ -2,8 +2,10 @@ package com.c332030.ctool4j.core.util;
 
 import com.c332030.ctool4j.core.classes.CObjUtils;
 import com.c332030.ctool4j.definition.function.CConsumer;
+import com.c332030.ctool4j.definition.function.CFunction;
 import com.c332030.ctool4j.definition.interfaces.ICValue;
 import lombok.experimental.UtilityClass;
+import lombok.val;
 
 /**
  * <p>
@@ -14,6 +16,11 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class CValueUtils {
+
+    public <E, T> T getValue(E obj, CFunction<E, ICValue<T>> function) {
+        val iValue = CObjUtils.convert(obj, function);
+        return getValue(iValue);
+    }
 
     public <T> T getValue(ICValue<T> iValue) {
         return CObjUtils.convert(iValue, ICValue::getValue);
