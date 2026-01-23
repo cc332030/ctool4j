72a25895b9b559142a7c305babf6da0350fcc789
c332030 <c332030@c332030.com>
2026-01-23 16:58:23 +0800

    feat: CSpringSecurityUtils

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/util/CSpringSecurityUtils.java
@@ -0,0 +1,41 @@
+package com.c332030.ctool4j.spring.util;
+
+import com.c332030.ctool4j.core.classes.CObjUtils;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * <p>
+ * Description: CSpringSecurityUtils
+ * </p>
+ *
+ * @since 2026/1/23
+ */
+@UtilityClass
+public class CSpringSecurityUtils {
+
+    public SecurityContext getContext() {
+        return SecurityContextHolder.getContext();
+    }
+
+    public Authentication getAuthentication() {
+        return getContext().getAuthentication();
+    }
+
+    public <T extends UserDetails> T getUserDetails() {
+
+        val authentication = getAuthentication();
+        if(authentication == null
+            || !authentication.isAuthenticated()
+        ) {
+            return null;
+        }
+
+        return CObjUtils.anyType(authentication.getPrincipal());
+    }
+
+}
