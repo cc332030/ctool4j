b6158eb3929d4e0e0df05747b79d1ece081f77bf
c332030 <c332030@c332030.com>
2026-01-24 14:11:57 +0800

    feat: ctool4j-spring-pom

--- ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/test/java/com/c332030/ctool4j/spring/test/util/CAutowiredUtilsBootTests.java
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/test/java/com/c332030/ctool4j/spring/test/util/CAutowiredUtilsBootTests.java
@@ -1,7 +1,7 @@
 package com.c332030.ctool4j.spring.test.util;
 
-import com.c332030.ctool4j.spring.test.annotation.CTool4jSpringBootTest;
 import com.c332030.ctool4j.spring.bean.CSpringConfigBeans;
+import com.c332030.ctool4j.spring.test.annotation.CTool4jSpringBootTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
