561b8482ada55770e14c8f6f44e571811a51dcc9
c332030 <c332030@c332030.com>
2026-01-24 14:33:24 +0800

    feat: EnableWebSecurity

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring-security/src/main/java/com/c332030/ctool4j/spring/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.c332030.ctool4j.spring.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * <p>
+ * Description: SecurityConfiguration
+ * </p>
+ *
+ * @since 2026/1/22
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+}
