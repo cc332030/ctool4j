1e31e6ed08220be788ba67bbd2f1f6b148aab21e
c332030 <c332030@c332030.com>
2026-01-26 19:17:06 +0800

    feat: 避免文档空指针

--- ctool4j-parent/ctool4j-component/ctool4j-doc-pom/ctool4j-doc-openapi2/src/main/java/com/c332030/ctool4j/doc/openapi2/configuration/COpenApi2Configuration.java
+++ ctool4j-parent/ctool4j-component/ctool4j-doc-pom/ctool4j-doc-openapi2/src/main/java/com/c332030/ctool4j/doc/openapi2/configuration/COpenApi2Configuration.java
@@ -6,13 +6,25 @@ import com.c332030.ctool4j.doc.openapi2.plugins.parameter.impl.CNotEmptyAnnotati
 import com.c332030.ctool4j.doc.openapi2.util.CSpringFoxUtils;
 import com.c332030.ctool4j.web.enums.CRequestHeaderEnum;
 import io.swagger.annotations.Api;
+import lombok.SneakyThrows;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.lang.NonNull;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -45,6 +57,42 @@ public class COpenApi2Configuration {
             .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
             .build()
             ;
+    }
+
+    /**
+     * 避免 springfox 报空指针
+     */
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                    .filter(mapping -> mapping.getPatternParser() == null)
+                    .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            @SneakyThrows
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                if (null == field) {
+                    return Collections.emptyList();
+                }
+                field.setAccessible(true);
+                return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+            }
+        };
     }
 
 }
