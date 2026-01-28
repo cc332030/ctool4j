7b08982efa5ef59ed203e5255216c8c514c0040e
c332030 <c332030@c332030.com>
2026-01-28 14:59:42 +0800

    feat: CSpringSecurityExceptionHandler

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring-security/src/main/java/com/c332030/ctool4j/spring/security/advice/CSpringSecurityExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.c332030.ctool4j.spring.security.advice;
+
+import com.c332030.ctool4j.definition.model.result.impl.CStrResult;
+import lombok.CustomLog;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * <p>
+ * Description: CGlobalExceptionHandler
+ * </p>
+ *
+ * @since 2025/10/27
+ */
+@CustomLog
+@Primary
+@ControllerAdvice
+public class CSpringSecurityExceptionHandler implements PriorityOrdered {
+
+    @Override
+    public int getOrder() {
+        return Integer.MIN_VALUE;
+    }
+
+    /**
+     * 兜底
+     */
+    @ResponseBody
+    @ExceptionHandler(AuthenticationException.class)
+    public CStrResult<Object> cHandleAuthenticationException(AuthenticationException e) {
+
+        log.debug("handle AuthenticationException", e);
+        return CStrResult.error(e.getMessage());
+    }
+
+    /**
+     * 未登录/登录过期
+     */
+    @ResponseBody
+    @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
+    public CStrResult<Object> cHandleAuthenticationCredentialsNotFoundException(AuthenticationCredentialsNotFoundException e) {
+
+        log.debug("handle AuthenticationCredentialsNotFoundException", e);
+        return CStrResult.error(e.getMessage());
+    }
+
+    /**
+     * 无权限
+     */
+    @ResponseBody
+    @ExceptionHandler(AccessDeniedException.class)
+    public CStrResult<Object> cHandleAccessDeniedException(AccessDeniedException e) {
+
+        log.debug("handle AccessDeniedException", e);
+        return CStrResult.error(e.getMessage());
+    }
+
+}
--- ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring-security/src/main/java/com/c332030/ctool4j/spring/security/configuration/CSecurityConfiguration.java
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring-security/src/main/java/com/c332030/ctool4j/spring/security/configuration/CSecurityConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 /**
  * <p>
@@ -43,6 +44,9 @@ public class CSecurityConfiguration {
         return authConfig.getAuthenticationManager();
     }
 
+    /**
+     * @see ExceptionTranslationFilter
+     */
     @Bean
     @ConditionalOnMissingBean(SecurityFilterChain.class)
     public SecurityFilterChain filterChain(
