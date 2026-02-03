53d13e996ebbb2fa55fcd786764207b79a2fb106
c332030 <c332030@c332030.com>
2026-02-03 13:08:01 +0800

    feat: 消息处理器，默认 utf-8

--- ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/util/CSpringHttpUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/util/CSpringHttpUtils.java
@@ -1,18 +1,18 @@
 package com.c332030.ctool4j.spring.util;
 
+import com.c332030.ctool4j.core.util.CJsonUtils;
 import com.c332030.ctool4j.core.util.CMap;
 import com.c332030.ctool4j.definition.function.CConsumer;
 import lombok.experimental.UtilityClass;
 import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 /**
  * <p>
@@ -45,6 +45,10 @@ public class CSpringHttpUtils {
     public void configureMessageConverters(Collection<HttpMessageConverter<?>> messageConverters) {
         messageConverters.forEach(messageConverter -> {
 
+            if(messageConverter instanceof AbstractHttpMessageConverter) {
+                configureAbstractHttpMessageConverter((AbstractHttpMessageConverter<?>) messageConverter);
+            }
+
             val configurer = MESSAGE_CONVERTER_CONFIGURER.get(messageConverter.getClass());
             if (configurer != null) {
                 configurer.accept(messageConverter);
@@ -54,21 +58,21 @@ public class CSpringHttpUtils {
     }
 
     private final Map<Class<?>, CConsumer<HttpMessageConverter<?>>> MESSAGE_CONVERTER_CONFIGURER = CMap.of(
-        StringHttpMessageConverter.class,
-        e -> configureStringHttpMessageConverter((StringHttpMessageConverter)e)
-        , MappingJackson2HttpMessageConverter.class,
+        MappingJackson2HttpMessageConverter.class,
         e -> configureJackson2HttpMessageConverter((MappingJackson2HttpMessageConverter)e)
     );
 
-    public void configureStringHttpMessageConverter(StringHttpMessageConverter messageConverter) {
-//        messageConverter.setDefaultCharset(StandardCharsets.UTF_8);
+    public void configureAbstractHttpMessageConverter(AbstractHttpMessageConverter<?> messageConverter) {
+
+        messageConverter.setDefaultCharset(StandardCharsets.UTF_8);
+
     }
 
     public void configureJackson2HttpMessageConverter(MappingJackson2HttpMessageConverter messageConverter) {
 
-//        val mediaTypes = new LinkedHashSet<>(messageConverter.getSupportedMediaTypes());
-//        mediaTypes.addAll(CJsonUtils.SUPPORT_MEDIA_TYPES);
-//        messageConverter.setSupportedMediaTypes(new ArrayList<>(mediaTypes));
+        val mediaTypes = new LinkedHashSet<>(messageConverter.getSupportedMediaTypes());
+        mediaTypes.addAll(CJsonUtils.SUPPORT_MEDIA_TYPES);
+        messageConverter.setSupportedMediaTypes(new ArrayList<>(mediaTypes));
 
     }
 
