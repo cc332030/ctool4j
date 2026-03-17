5dd901dcc8ac8212746e3a587bc09d1e6dc2b785
c332030 <c332030@c332030.com>
2026-03-17 16:36:08 +0800

    feat: CCountryCodeEnum

---
+++ ctool4j-parent/ctool4j-component/ctool4j-definition/src/main/java/com/c332030/ctool4j/definition/constant/business/CCountryCodeEnum.java
@@ -0,0 +1,32 @@
+package com.c332030.ctool4j.definition.constant.business;
+
+import com.c332030.ctool4j.definition.interfaces.ICValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * <p>
+ * Description: CCountryCodeEnum
+ * </p>
+ *
+ * @since 2026/3/17
+ */
+@Getter
+@AllArgsConstructor
+public enum CCountryCodeEnum implements ICValue<Integer> {
+
+    CHINA(86, "中国"),
+
+    ;
+
+    /**
+     * 描述
+     */
+    final Integer value;
+
+    /**
+     * 描述
+     */
+    final String text;
+
+}
