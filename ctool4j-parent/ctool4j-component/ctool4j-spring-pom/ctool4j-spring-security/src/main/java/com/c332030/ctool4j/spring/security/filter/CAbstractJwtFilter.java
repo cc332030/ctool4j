cd3ea17707fbdeeba7d00da5a17ce32d34779f74
c332030 <c332030@c332030.com>
2026-03-19 14:32:40 +0800

    feat: CAbstractJwtFilter

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring-security/src/main/java/com/c332030/ctool4j/spring/security/filter/CAbstractJwtFilter.java
@@ -0,0 +1,14 @@
+package com.c332030.ctool4j.spring.security.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * <p>
+ * Description: CAbstractJwtFilter jwt 过滤器
+ * </p>
+ *
+ * @since 2026/3/19
+ */
+public abstract class CAbstractJwtFilter extends OncePerRequestFilter {
+
+}
