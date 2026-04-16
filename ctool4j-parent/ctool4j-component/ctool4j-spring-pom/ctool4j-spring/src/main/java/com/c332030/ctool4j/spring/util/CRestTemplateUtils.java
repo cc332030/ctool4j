36c2ba57fadaa997a5ddc6b1d5c8b7ce4d042389
c332030 <c332030@c332030.com>
2026-04-16 11:06:05 +0800

    feat: RestTemplate

--- ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/util/CRestTemplateUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/main/java/com/c332030/ctool4j/spring/util/CRestTemplateUtils.java
@@ -1,6 +1,12 @@
 package com.c332030.ctool4j.spring.util;
 
+import com.c332030.ctool4j.core.jackson.CJacksonUtils;
+import com.c332030.ctool4j.core.util.CHttpClientUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.experimental.UtilityClass;
+import lombok.val;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * <p>
@@ -11,5 +17,22 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class CRestTemplateUtils {
+
+    public final RestTemplate REST_TEMPLATE = restTemplate(CJacksonUtils.OBJECT_MAPPER);
+
+    public RestTemplate restTemplate(ObjectMapper objectMapper) {
+
+        val restTemplate = new RestTemplate(CHttpClientUtils.REQUEST_FACTORY);
+        val messageConverters = restTemplate.getMessageConverters();
+        CSpringHttpUtils.configureMessageConverters(messageConverters);
+
+        messageConverters.forEach(messageConverter -> {
+            if (messageConverter instanceof MappingJackson2HttpMessageConverter) {
+                ((MappingJackson2HttpMessageConverter) messageConverter).setObjectMapper(objectMapper);
+            }
+        });
+
+        return restTemplate;
+    }
 
 }
