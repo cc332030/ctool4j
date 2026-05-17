037edb2cf49cbc561f7ab37a3c97a7dbb3ca6c82
c332030 <c332030@c332030.com>
2026-05-17 18:21:14 +0800

    feat: CFeignConfig

---
+++ ctool4j-parent/ctool4j-component/ctool4j-feign/src/main/java/com/c332030/ctool4j/feign/config/CFeignConfig.java
@@ -0,0 +1,32 @@
+package com.c332030.ctool4j.feign.config;
+
+import com.c332030.ctool4j.core.util.CMap;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * Description: CFeignConfig
+ * </p>
+ *
+ * @since 2025/9/21
+ */
+@Data
+@ConfigurationProperties("feign")
+public class CFeignConfig {
+
+    /**
+     * 客户端信息
+     */
+    Map<String, ClientConfig> client = CMap.of();
+
+}
+
+@Data
+class ClientConfig {
+
+    String url;
+
+}
