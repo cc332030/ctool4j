8105d7e6e1a645dbbae445a69e11fa80b5211ff9
c332030 <c332030@c332030.com>
2026-05-19 11:19:38 +0800

    feat: CAutowiredScan

--- ctool4j-processor-pom/ctool4j-autowired-processor/src/main/java/com/c332030/ctool4j/spring/annotation/CAutowiredScan.java
+++ ctool4j-processor-pom/ctool4j-autowired-processor/src/main/java/com/c332030/ctool4j/spring/annotation/CAutowiredScan.java
@@ -1,10 +1,13 @@
 package com.c332030.ctool4j.spring.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * <p>
- * Description: CAutowired
+ * Description: CAutowiredScan
  * </p>
  *
  * @since 2025/12/23
