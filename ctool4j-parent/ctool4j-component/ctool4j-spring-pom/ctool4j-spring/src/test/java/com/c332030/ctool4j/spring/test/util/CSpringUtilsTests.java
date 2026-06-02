cadc3abc1b27e525909e952b7c80e285a8c3517a
c332030 <c332030@c332030.com>
2026-06-02 10:08:55 +0800

    feat: profilePrefix

---
+++ ctool4j-parent/ctool4j-component/ctool4j-spring-pom/ctool4j-spring/src/test/java/com/c332030/ctool4j/spring/test/util/CSpringUtilsTests.java
@@ -0,0 +1,59 @@
+package com.c332030.ctool4j.spring.test.util;
+
+import com.c332030.ctool4j.core.enums.CProfileEnum;
+import com.c332030.ctool4j.spring.test.annotation.CTool4jSpringBootTest;
+import com.c332030.ctool4j.spring.util.CSpringUtils;
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p>
+ * Description: CSpringUtilsTests
+ * </p>
+ *
+ * @since 2026/6/2
+ */
+@CTool4jSpringBootTest
+public class CSpringUtilsTests {
+
+    private static final CProfileEnum PROFILE = CProfileEnum.DEV;
+
+    private static final String CLASS_NAME = CSpringUtilsTests.class.getSimpleName();
+
+    @Test
+    public void getActiveProfile() {
+
+        val profile = CSpringUtils.getActiveProfile();
+        Assertions.assertEquals(PROFILE, profile);
+    }
+
+    @Test
+    public void getActiveProfileText() {
+
+        val profile = CSpringUtils.getActiveProfileText();
+        Assertions.assertEquals(PROFILE.getText(), profile);
+    }
+
+    @Test
+    public void profilePrefix() {
+
+        val profile = CSpringUtils.profilePrefix(CLASS_NAME);
+        Assertions.assertEquals(PROFILE.name() + CLASS_NAME, profile);
+    }
+
+    @Test
+    public void profilePrefixExcludeProd() {
+
+        val profile = CSpringUtils.profilePrefixExcludeProd(CLASS_NAME);
+        Assertions.assertEquals(PROFILE.name() + CLASS_NAME, profile);
+    }
+
+    @Test
+    public void profileTextSuffix() {
+
+        val profile = CSpringUtils.profileTextSuffix(CLASS_NAME);
+        Assertions.assertEquals(CLASS_NAME + "-" + PROFILE.getText(), profile);
+    }
+
+}
