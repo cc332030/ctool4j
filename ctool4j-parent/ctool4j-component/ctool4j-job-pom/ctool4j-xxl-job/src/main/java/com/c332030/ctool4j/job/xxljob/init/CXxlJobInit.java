f5219d3509f1b8c9ac44dfee767ec606db989fc1
c332030 <c332030@c332030.com>
2026-06-04 16:30:57 +0800

    feat: 创建日志文件夹

---
+++ ctool4j-parent/ctool4j-component/ctool4j-job-pom/ctool4j-xxl-job/src/main/java/com/c332030/ctool4j/job/xxljob/init/CXxlJobInit.java
@@ -0,0 +1,50 @@
+package com.c332030.ctool4j.job.xxljob.init;
+
+import cn.hutool.core.util.StrUtil;
+import com.c332030.ctool4j.job.xxljob.config.CXxlJobExecutorConfig;
+import com.c332030.ctool4j.spring.lifecycle.ICSpringInit;
+import lombok.AllArgsConstructor;
+import lombok.CustomLog;
+import lombok.val;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+/**
+ * <p>
+ * Description: CXxlJobInit
+ * </p>
+ *
+ * @since 2026/6/4
+ */
+@CustomLog
+@Component
+@AllArgsConstructor
+public class CXxlJobInit implements ICSpringInit {
+
+    CXxlJobExecutorConfig executorConfig;
+
+    @Override
+    public void onInit() {
+
+        try {
+
+            val logPath = executorConfig.getLogpath();
+            if(StrUtil.isBlank(logPath)) {
+                return;
+            }
+
+            val file = new File(logPath);
+            if(file.exists()) {
+                return;
+            }
+
+            log.info("创建日志文件夹，logPath: {}，结果：{}", logPath, file.mkdirs());
+
+        } catch (Exception e) {
+            log.error("CXxlJobInit error", e);
+        }
+
+    }
+
+}
