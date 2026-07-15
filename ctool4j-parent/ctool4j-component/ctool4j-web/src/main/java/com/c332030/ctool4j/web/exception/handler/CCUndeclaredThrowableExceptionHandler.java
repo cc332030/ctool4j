7349939c7a475a8fb8edb71656d56a5ff09a4c41
c332030 <c332030@c332030.com>
2026-07-15 15:54:18 +0800

    feat: CCUndeclaredThrowableExceptionHandler

---
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/exception/handler/CCUndeclaredThrowableExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.c332030.ctool4j.web.exception.handler;
+
+import com.c332030.ctool4j.definition.model.result.impl.CStrResult;
+import com.c332030.ctool4j.spring.util.CRequestUtils;
+import com.c332030.ctool4j.web.exception.annotation.ConditionalOnMissingExceptionHandler;
+import lombok.CustomLog;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.UndeclaredThrowableException;
+
+/**
+ * <p>
+ * Description: CThrowableHandler
+ * </p>
+ *
+ * @since 2026/4/9
+ */
+@CustomLog
+@RestControllerAdvice
+@ConditionalOnMissingExceptionHandler(UndeclaredThrowableException.class)
+public class CCUndeclaredThrowableExceptionHandler {
+
+    @ExceptionHandler(UndeclaredThrowableException.class)
+    public CStrResult<Void> handle(UndeclaredThrowableException e) {
+
+        log.error("handle UndeclaredThrowableException，requestURI: {}", CRequestUtils.getRequestURIDefaultNull(), e.getCause());
+        return CStrResult.error("未知异常");
+    }
+
+}
