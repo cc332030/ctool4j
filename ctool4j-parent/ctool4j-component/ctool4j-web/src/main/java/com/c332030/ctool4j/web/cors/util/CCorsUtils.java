4d1e6d335f2b8ea9bb07cc867b24e7117a0566f1
c332030 <c332030@c332030.com>
2026-08-14 15:34:09 +0800

    feat: 细节修复

--- ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/cors/util/CCorsUtils.java
+++ ctool4j-parent/ctool4j-component/ctool4j-web/src/main/java/com/c332030/ctool4j/web/cors/util/CCorsUtils.java
@@ -96,6 +96,7 @@ public class CCorsUtils {
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, headers);
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        // 允许当前请求方法类型
         response.setHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, method);
 
     }
