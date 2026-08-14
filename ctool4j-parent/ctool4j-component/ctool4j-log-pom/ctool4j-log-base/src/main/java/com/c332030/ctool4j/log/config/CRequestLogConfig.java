f9067ee71310b9225a09526779f92dd99cd866fe
c332030 <c332030@c332030.com>
2026-08-14 11:08:18 +0800

    feat: 请求头日志开关

--- ctool4j-parent/ctool4j-component/ctool4j-log-pom/ctool4j-log-base/src/main/java/com/c332030/ctool4j/log/config/CRequestLogConfig.java
+++ ctool4j-parent/ctool4j-component/ctool4j-log-pom/ctool4j-log-base/src/main/java/com/c332030/ctool4j/log/config/CRequestLogConfig.java
@@ -22,6 +22,13 @@ public class CRequestLogConfig {
     Boolean enable = false;
 
     /**
+     * 请求头日志开关
+     * <p>默认关闭：请求头可能含 Authorization、Cookie 等敏感信息，需要时显式开启；
+     * token 等业务数据不受此开关影响，仍由业务数据区输出</p>
+     */
+    Boolean enableHeader = false;
+
+    /**
      * 慢请求日志-开关
      */
     Boolean slowLogEnable = true;
