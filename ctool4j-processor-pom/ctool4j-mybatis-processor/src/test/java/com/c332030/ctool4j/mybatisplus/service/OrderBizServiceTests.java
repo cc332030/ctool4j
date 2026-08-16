275503a7228dc00b8f6993217b8ce2075ef8f74c
c332030 <c332030@c332030.com>
2026-08-16 10:55:52 +0800

    feat: 细节调整

---
+++ ctool4j-processor-pom/ctool4j-mybatis-processor/src/test/java/com/c332030/ctool4j/mybatisplus/service/OrderBizServiceTests.java
@@ -0,0 +1,27 @@
+package com.c332030.ctool4j.mybatisplus.service;
+
+import com.c332030.ctool4j.spring.test.annotation.CTool4jSpringBootTest;
+import lombok.CustomLog;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * <p>
+ * Description: OrderBizServiceTests
+ * </p>
+ *
+ * @since 2026/8/16
+ */
+@CustomLog
+@CTool4jSpringBootTest
+public class OrderBizServiceTests {
+
+    @Autowired
+    OrderBizService orderBizService;
+
+    @Test
+    void run(){
+
+    }
+
+}
